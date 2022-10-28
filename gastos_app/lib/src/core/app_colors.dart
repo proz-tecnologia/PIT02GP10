cb4891dc9562733ff96fb15de80da8def83d803c
paahdiego <paah-diego@hotmail.com>
2022-10-27 21:07:27 -0300

    classe de cores criada

---
+++ gastos_app/lib/src/core/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF619B8A);
+  static const Color secondaryColor = Color(0xFFA1C181);
+  static const Color backgroundColor = Color(0xFF233D4D);
+  static const Color fontColor = Color(0xFFEDF4EF);
+
+  static const Color orange = Color(0xFFFE7F2D);
+  static const Color yellow = Color(0xFFFCCA46);
+}
