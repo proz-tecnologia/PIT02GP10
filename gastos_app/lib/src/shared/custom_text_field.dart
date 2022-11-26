66d02c0a06bf89483704694222df523a69719dbf
Marcelle Souza <marcelleflavya@gmail.com>
2022-11-25 21:48:06 -0300

    Concluded Custom Text Field

--- gastos_app/lib/src/shared/custom_text_field.dart
+++ gastos_app/lib/src/shared/custom_text_field.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
+
 import 'package:gastos_app/src/core/app_colors.dart';
 
-class CostumTextField extends StatefulWidget {
-  const CostumTextField({
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
     Key? key,
-    this.label,
+    required this.label,
     required this.filledColor,
     this.suffixIcon,
+    this.textInputType,
   }) : super(key: key);
 
-  final String? label;
-  final Color ? filledColor;
+  final String label;
+  final Color? filledColor;
   final Widget? suffixIcon;
+  final TextInputType? textInputType;
 
   @override
-  State<CostumTextField> createState() => _CostumTextFieldState();
+  State<CustomTextField> createState() => _CustomTextFieldState();
 }
 
-class _CostumTextFieldState extends State<CostumTextField> {
+class _CustomTextFieldState extends State<CustomTextField> {
   final BorderRadius _borderRadius =
       const BorderRadius.all(Radius.circular(20.0));
 
@@ -27,47 +30,43 @@ class _CostumTextFieldState extends State<CostumTextField> {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            focusNode.hasFocus && widget.label != null
-            ? Text(widget.label !) 
-            : Text(""),
-            Stack(
-              children: [
-                TextFormField(
-                  focusNode: focusNode,
-                  onTap: () {
-                    setState(() {});
-                    if (focusNode.hasFocus) focusNode.unfocus();
-                  },
-                  decoration: InputDecoration(
-                      labelStyle: TextStyle(
-                        fontSize: 18,
-                        color: AppColors.fontColor,
-                      ),
-                      fillColor: AppColors.secondaryColor,
-                      filled: true,
-                      border: OutlineInputBorder(borderRadius: _borderRadius)),
-                  keyboardType: TextInputType.name,
-                ),
-                if (!focusNode.hasFocus)
-                  Positioned(
-                    top: 0,
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    child: GestureDetector(
-                      onTap: () {
-                        if (focusNode.hasFocus) focusNode.unfocus();
-                      },
-                      child: Center(child: Text(widget.label!)),
-                    ),
+        child: InkWell(
+          onTap: () {
+            if (focusNode.hasFocus) focusNode.unfocus();
+            setState(() {});
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(focusNode.hasFocus ? widget.label : ""),
+              Stack(
+                children: [
+                  TextFormField(
+                    focusNode: focusNode,
+                    decoration: InputDecoration(
+                        labelStyle: TextStyle(
+                          fontSize: 18,
+                          color: AppColors.fontColor,
+                        ),
+                        fillColor: AppColors.secondaryColor,
+                        filled: true,
+                        border:
+                            OutlineInputBorder(borderRadius: _borderRadius)),
+                    keyboardType: widget.textInputType,
                   ),
-              ],
-            ),
-          ],
+                  if (!focusNode.hasFocus)
+                    Positioned(
+                      top: 0,
+                      bottom: 0,
+                      right: 0,
+                      left: 0,
+                      child: Center(child: Text(widget.label)),
+                    ),
+                ],
+              ),
+            ],
+          ),
         )
     );
   }
