62dbefcd71d011365b1eff3d9a779dc2dda46e33
Marcelle Souza <marcelleflavya@gmail.com>
2022-12-01 22:28:01 -0300

    Improvements create expense page

--- gastos_app/lib/src/modules/home/expense/pages/create_expense_page.dart
+++ gastos_app/lib/src/modules/home/expense/pages/create_expense_page.dart
@@ -1,13 +1,31 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:gastos_app/src/core/app_colors.dart';
+import 'package:gastos_app/src/modules/home/expense/pages/controllers/create_expense_controller.dart';
 import 'package:gastos_app/src/shared/components/custom_date_picker.dart';
 import 'package:gastos_app/src/shared/components/custom_elevated_button.dart';
+import 'package:gastos_app/src/shared/components/custom_loading_icon.dart';
 import 'package:gastos_app/src/shared/components/custom_text_field.dart';
+import 'package:validatorless/validatorless.dart';
 
-class CreateExpensePage extends StatelessWidget {
+class CreateExpensePage extends StatefulWidget {
   const CreateExpensePage({super.key});
+
+  @override
+  State<CreateExpensePage> createState() => _CreateExpensePageState();
+}
+
+class _CreateExpensePageState extends State<CreateExpensePage> {
+  final formKey = GlobalKey<FormState>();
+  final createExpenseController = CreateExpenseController();
+
+  final valueController = MoneyMaskedTextController(
+    leftSymbol: "R\$",
+    decimalSeparator: ',',
+    thousandSeparator: '.',
+  );
+  final descriptionController = TextEditingController();
+  DateTime? chosenDate;
 
   @override
   Widget build(BuildContext context) {
@@ -34,47 +52,84 @@ class CreateExpensePage extends StatelessWidget {
                 "Nova despesa",
                 style: textStyle,
               ),
-              const SizedBox(height: 40),
-              const CustomTextField(
-                label: "Valor da despesa",
-                filledColor: AppColors.expenseColor,
-              ),
-              const SizedBox(height: 20),
-              const CustomTextField(
-                label: "Descrição",
-                filledColor: AppColors.expenseColor,
-              ),
+             const SizedBox(height: 40),
+                CustomTextField(
+                  label: "Valor da despesa",
+                  filledColor: AppColors.expenseColor,
+                  controller: valueController,
+                  textInputType: TextInputType.number,
+                  textInputAction: TextInputAction.next,
+                  validator: Validatorless.multiple([
+                    Validatorless.required("Necessário informar um valor"),
+                    (String? value) {
+                      if (value == null || value == "R\$ 0,00") {
+                        return "Digite um valor válido";
+                      }
+                      return null;
+                    },
+                  ]),
+                ),
+               const SizedBox(height: 20),
+                CustomTextField(
+                  label: "Descrição",
+                  filledColor: AppColors.expenseColor,
+                  controller: descriptionController,
+                  textInputAction: TextInputAction.next,
+                  validator: Validatorless.required(
+                    "Necessário informar uma descrição",
+                  ),
+                ),
               const SizedBox(height: 20),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: CustomDatePicker(
                   label: "Data de vencimento",
                   onDateChange: (date) {
-                    log(date.toIso8601String());
+                    chosenDate = date;
                   },
+                  validator: Validatorless.required(
+                    "Necessário informar uma data",
+                  ),
                   filledColor: AppColors.expenseColor,
                 ),
               ),
               const SizedBox(height: 150),
-              SizedBox(
-                width: 110,
-                child: CustomElevatedButton(
-                  backgroundColor: AppColors.expenseColor,
-                  foregroundColor: AppColors.backgroundColor,
-                  onPressed: () {},
-                  child: Text(
-                    "Finalizar",
-                    style: textStyle?.copyWith(
-                      fontSize: 16,
-                      color: AppColors.backgroundColor,
-                    ),
-                  ),
+                ValueListenableBuilder<CreateExpenseStates>(
+                  valueListenable:
+                      createExpenseController.createExpenseStateNotifier,
+                  builder: (context, state, _) {
+                    return SizedBox(
+                      width: 110,
+                      child: CustomElevatedButton(
+                        backgroundColor: AppColors.expenseColor,
+                        foregroundColor: AppColors.backgroundColor,
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            createExpenseController.createExpense(
+                              title: descriptionController.text,
+                              value: valueController.numberValue,
+                            );
+                          }
+                        },
+                        child: state == CreateExpenseStates.loading
+                            ? const CustomLoadingIcon(size: 16)
+                            : Text(
+                                "Finalizar",
+                                style: textStyle?.copyWith(
+                                  fontSize: 16,
+                                  color: AppColors.backgroundColor,
+                                ),
+                              ),
+                      ),
+                    );
+                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
+
+             
