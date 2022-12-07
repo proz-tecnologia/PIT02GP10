e7aecb22cdb5931eb959b5028c826c30f7082386
felype.santos <felypefas@gmail.com>
2022-12-07 14:45:40 -0300

    criado a classe PasswordField

---
+++ gastos_app/lib/src/shared/components/password_field.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gastos_app/src/core/app_colors.dart';
+import 'package:gastos_app/src/core/app_themes.dart';
+import 'package:gastos_app/src/shared/components/effectless_inkwell.dart';
+
+class PasswordTextField extends StatefulWidget {
+  const PasswordTextField({
+    Key? key,
+    required this.label,
+    this.filledColor = AppColors.secondaryColor,
+    this.textInputType = TextInputType.text,
+    this.textInputAction = TextInputAction.done,
+    this.suffixIcon,
+    this.validator,
+    this.onFieldSubmitted,
+    this.onTap,
+    this.focusNode,
+    this.controller,
+    this.readOnly = false,
+    this.inputFormatters,
+  }) : super(key: key);
+
+  final String label;
+  final Color filledColor;
+
+  final TextInputType textInputType;
+  final TextInputAction textInputAction;
+  final Widget? suffixIcon;
+  final FormFieldValidator<String>? validator;
+  final void Function(String)? onFieldSubmitted;
+  final VoidCallback? onTap;
+  final FocusNode? focusNode;
+  final TextEditingController? controller;
+  final bool readOnly;
+  final List<TextInputFormatter>? inputFormatters;
+
+  @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  late FocusNode focusNode;
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    focusNode = widget.focusNode ?? FocusNode();
+    controller = widget.controller ?? TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final focusedLabelStyle = Theme.of(context).textTheme.bodyText1?.copyWith(
+          fontSize: 15,
+          color: AppColors.fontColor,
+          fontWeight: FontWeight.w500,
+        );
+
+    final unfocusedLabelStyle = Theme.of(context).textTheme.bodyText1?.copyWith(
+          fontSize: 15,
+          color: AppColors.backgroundColor,
+          fontWeight: FontWeight.w500,
+        );
+
+    final valueTextStyle = Theme.of(context).textTheme.bodyText1?.copyWith(
+          fontSize: 15,
+          color: AppColors.backgroundColor,
+          fontWeight: FontWeight.w500,
+        );
+
+    final outlineBorder = OutlineInputBorder(
+      borderRadius: AppThemes.defaultBorderRadius,
+      borderSide: BorderSide(
+        color: widget.filledColor,
+      ),
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          focusNode.hasPrimaryFocus || controller.text.isNotEmpty
+              ? widget.label
+              : "",
+          style: focusedLabelStyle,
+        ),
+        const SizedBox(height: 2),
+        Stack(
+          children: [
+            TextFormField(
+              onTap: () {
+                setState(() {
+                  if (focusNode.hasPrimaryFocus) focusNode.unfocus();
+                });
+                if (widget.onTap != null) widget.onTap!();
+              },
+              controller: controller,
+              focusNode: focusNode,
+              onChanged: (value) {
+                if (value.isEmpty) setState(() {});
+              },
+              cursorColor: AppColors.backgroundColor,
+              keyboardType: widget.textInputType,
+              textInputAction: widget.textInputAction,
+              validator: widget.validator,
+              readOnly: widget.readOnly,
+              style: valueTextStyle,
+              inputFormatters: widget.inputFormatters,
+              onFieldSubmitted: widget.onFieldSubmitted,
+              decoration: InputDecoration(
+                fillColor: widget.filledColor,
+                filled: true,
+                border: outlineBorder,
+                enabledBorder: outlineBorder,
+                focusedBorder: outlineBorder,
+                suffixIcon: widget.suffixIcon,
+                errorMaxLines: 1,
+                errorStyle: focusedLabelStyle?.copyWith(
+                  color: AppColors.errorColor,
+                ),
+                disabledBorder: outlineBorder.copyWith(
+                  borderSide: const BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                errorBorder: outlineBorder.copyWith(
+                  borderSide: const BorderSide(
+                    color: AppColors.errorColor,
+                  ),
+                ),
+              ),
+            ),
+            if (!focusNode.hasPrimaryFocus && controller.text.isEmpty)
+              Positioned(
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: EffectlessInkWell(
+                  onTap: () {
+                    setState(() {
+                      if (!focusNode.hasPrimaryFocus) {
+                        focusNode.requestFocus();
+                      }
+                    });
+                  },
+                  child: Center(
+                    child: Text(
+                      widget.label,
+                      style: unfocusedLabelStyle,
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ],
+    );
+  }
+}
