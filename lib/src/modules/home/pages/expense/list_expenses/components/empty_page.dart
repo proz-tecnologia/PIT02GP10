a4c7d87b08ad39edd42f071a3cc8707774782aaa
Marcelle Souza <marcelleflavya@gmail.com>
2023-01-12 15:46:32 -0300

    possibility to create expenses from the empty list

--- lib/src/modules/home/pages/expense/list_expenses/components/empty_page.dart
+++ lib/src/modules/home/pages/expense/list_expenses/components/empty_page.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
 import 'package:gastos_app/src/core/app_colors.dart';
+import 'package:gastos_app/src/modules/home/home_routes.dart';
 import 'package:gastos_app/src/shared/components/custom_elevated_button.dart';
 
 class EmptyPage extends StatelessWidget {
@@ -34,7 +38,14 @@ class EmptyPage extends StatelessWidget {
         ),
         const SizedBox(height: 20),
         CustomElevatedButton(
-          onPressed: onCreateExpense,
+          onPressed: (() {
+            Modular.to.pushNamed(HomeRoutes.createExpense).then((value) {
+              if (value == true) {
+                Modular.to.pushNamed(HomeRoutes.listExpense);
+              }
+              Modular.to.pop(onCreateExpense);
+            });
+          }),
           backgroundColor: AppColors.expenseColor,
           child: const Text("Cadastrar despesa"),
         ),
