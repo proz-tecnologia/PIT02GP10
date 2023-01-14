f277bbb7254e75327db514e58940287891d5323e
paahdiego <paah-diego@hotmail.com>
2023-01-13 21:21:32 -0300

    added crashlytics setup

--- lib/src/firebase/firebase_controller.dart
+++ lib/src/firebase/firebase_controller.dart
@@ -1,10 +1,43 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:gastos_app/firebase_options.dart';
 
 class FirebaseController {
   Future<void> setup() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+    runZonedGuarded<Future<void>>(
+      () async {
+        await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
+
+        await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(
+          true,
+        );
+
+        FlutterError.onError = (errorDetails) {
+          FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+        };
+
+        PlatformDispatcher.instance.onError = (error, stack) {
+          FirebaseCrashlytics.instance.recordError(error, stack);
+          return true;
+        };
+
+        Isolate.current.addErrorListener(RawReceivePort((pair) async {
+          final List<dynamic> errorAndStacktrace = pair;
+          await FirebaseCrashlytics.instance.recordError(
+            errorAndStacktrace.first,
+            errorAndStacktrace.last,
+          );
+        }).sendPort);
+      },
+      (error, stack) {
+        FirebaseCrashlytics.instance.recordError(error, stack);
+      },
     );
   }
 }
