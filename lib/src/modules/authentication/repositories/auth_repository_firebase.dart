8c50ff16a23c52a060ec1a2dcc4ebd9983288c37
Marcelle Souza <marcelleflavya@gmail.com>
2023-01-13 22:32:20 -0300

    modified auth repository firebase

--- lib/src/modules/authentication/repositories/auth_repository_firebase.dart
+++ lib/src/modules/authentication/repositories/auth_repository_firebase.dart
@@ -51,4 +51,26 @@ class AuthRepositoryFirebase implements AuthRepository {
 
   @override
   User? get currentUser => firebaseAuthInstance.currentUser;
+
+  @override
+  Future<void> updateUser({
+    String? name,
+    String? customName,
+    String? phone,
+    String? password,
+  }) async {
+    if (name != null) {
+      await firebaseAuthInstance.currentUser!.updateDisplayName(name);
+    }
+    {
+      if (phone != null) {
+        //await firebaseAuthInstance.currentUser!.updatePhoneNumber(phone);
+      }
+      {
+        if (password != null) {
+          await firebaseAuthInstance.currentUser!.updateDisplayName(password);
+        }
+      }
+    }
+  }
 }
