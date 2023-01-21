655aded9c440a2f88c60fddd25817e5e5642b842
Marcelle Souza <marcelleflavya@gmail.com>
2023-01-21 16:59:01 -0300

    test

---
+++ test/mock_classes.dart
@@ -0,0 +1,4 @@
+import 'package:gastos_app/src/modules/authentication/repositories/auth_repository_firebase.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepositoryFirebase extends Mock implements AuthRepositoryFirebase {}
