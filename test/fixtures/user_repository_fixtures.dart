d63ef8373b9db435b92907bacc14c34472559f00
paahdiego <paah-diego@hotmail.com>
2023-01-22 18:52:36 -0300

    uploading user repository tests

--- test/fixtures/user_repository_fixtures.dart
+++ test/fixtures/user_repository_fixtures.dart
@@ -5,6 +5,9 @@ class UserRepositoryFixtures {
     email: faker.internet.email(),
     id: faker.guid.random.string(50),
     name: faker.person.name(),
+    nickname: faker.person.firstName(),
+    avatarUrl: faker.lorem.word(),
+    newName: faker.person.firstName(),
   );
 }
 
@@ -12,9 +15,16 @@ class UserRepositoryTestData {
   final String email;
   final String id;
   final String name;
+  final String nickname;
+  final String avatarUrl;
+  final String newName;
+
   UserRepositoryTestData({
     required this.email,
     required this.id,
     required this.name,
+    required this.nickname,
+    required this.avatarUrl,
+    required this.newName,
   });
 }
